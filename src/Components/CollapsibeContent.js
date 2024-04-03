d18e55f5882f624ef8f02f71961e94e1542230d7
Matthew Lee <mlee164@lion.lmu.edu>
2024-04-03 15:14:28 -0700

    Resume Edits

--- src/Components/CollapsibeContent.js
+++ src/Components/CollapsibeContent.js
@@ -11,12 +11,13 @@ function CollapsibleContent({ buttonLabel, bulletPoints }) {
   return (
     <div>
       <div style={{ border: '2px solid black', borderRadius: '20px', maxWidth: '300px', margin: '0 auto' }}>
-        <button type="button" className={`collapsible ${isOpen ? 'active' : ''}`} onClick={toggleContent}>
+        <button type="button" className={`collapsible ${isOpen ? 'active' : ''}`} onClick={toggleContent} style={{ borderRadius: '20px',
+         width: '100%', padding: '10px', background: '#777', color: 'white', border: 'none', outline: 'none', cursor: 'pointer' }}>
           <div style={{textAlign: 'center'}}>
             {buttonLabel}
           </div>
         </button>
-        <div className="content" style={{ display: isOpen ? 'block' : 'none' }}>
+        <div className="content" style={{ display: isOpen ? 'block' : 'none', padding: '10px' }}>
           <ul>
             {bulletPoints.map((point, index) => (
               <li key={index}>{point}</li>
@@ -29,4 +30,3 @@ function CollapsibleContent({ buttonLabel, bulletPoints }) {
 }
 
 export default CollapsibleContent;
-
