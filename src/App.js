5afda4ff1c24715d56efa97f443c0af07ae5e444
Matthew Lee <mlee164@lion.lmu.edu>
2024-04-05 13:50:51 -0700

    Trying Threejs

--- src/App.js
+++ src/App.js
@@ -4,17 +4,21 @@ import HomePage from './Pages/HomePage';
 import ResumePage from './Pages/ResumePage';
 import ContactsPage from './Pages/ContactsPage';
 import NavBar from './Components/NavBar';
+import ThreeScene from './Components/ThreeScene'; // Import the ThreeScene component
 
 function App() {
   return (
     <Router>
-      <div style={{ backgroundColor: '#9dc183', height: '90px', width: '100%', position: 'absolute', top: '0', left: '0' }}></div>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/second-page" element={<ResumePage />} />
-        <Route path="/contacts" element={<ContactsPage />} />
-      </Routes>
+      <div>
+        <div style={{ backgroundColor: '#9dc183', height: '90px', width: '100%', position: 'absolute', top: '0', left: '0' }}></div>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/second-page" element={<ResumePage />} />
+          <Route path="/contacts" element={<ContactsPage />} />
+          <Route path="/three-scene" element={<ThreeScene />} />
+        </Routes>
+      </div>
     </Router>
   );
 }
--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,20 +1,12 @@
 import React from 'react';
-import tattoo from '../Images/tattoo.png';
+import Profile from '../Images/LinkedIn.jpeg';
 import '../App.css';
 
 function HomePage() {
     return (
-        <div>
-            <div style={{ textAlign: 'center', paddingTop: '25px' }}>
-                <h1>
-                    Welcome to the Home Page!
-                </h1>
-                <div style={{ borderRadius: '10px', overflow: 'hidden', display: 'inline-block', border: '2px solid black' }}>
-                    <div style={{ borderRadius: '8px', overflow: 'hidden', border: '2px solid black' }}>
-                        <img src={tattoo} alt="tattoo" style={{ width: '200px' }} />
-                    </div>
-                </div>
-                <p>yippee</p>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '50vh' }}>
+            <div style={{ borderRadius: '50%', overflow: 'hidden', border: '4px solid black', width: '300px', height: '300px', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                <img src={Profile} alt="Profile" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
             </div>
         </div>
     )
